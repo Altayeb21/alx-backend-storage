48d3dda9bf50aba5755f3a7acf460c43b811b922
Altayeb Mohamed <140106295+Altayeb21@users.noreply.github.com>
2024-10-22 06:55:52 +0200

    Add files via upload

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" nginx logs """
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    reqs = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    req_dic = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    print(f"{collection.count_documents({})} logs")
+    pipeline = [
+        {'$match': {'method': {'$in': reqs}}},
+        {'$group': {'_id': '$method', 'sum': {'$sum': 1}}}
+    ]
+    print("Methods:")
+    result = collection.aggregate(pipeline)
+    for doc in result:
+        if doc['_id'] in reqs:
+            req_dic[doc['_id']] = doc['sum']
+    for req in reqs:
+        print(f"\tmethod {req}: {req_dic[req]}")
+    st_checks = collection.count_documents({"method": 'GET',
+                                            "path": "/status"})
+    print(f"{st_checks} status check")
+    print("IPs:")
+    ips = collection.aggregate([{'$group': {'_id': '$ip', 'count': {'$sum': 1}}}, {'$sort': {'count': -1}}, {'$limit': 10}])
+    for doc in ips:
+        print(f"\t{doc['_id']}: {doc['count']}")
---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" nginx logs """
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    reqs = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    req_dic = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    print(f"{collection.count_documents({})} logs")
+    pipeline = [
+        {'$match': {'method': {'$in': reqs}}},
+        {'$group': {'_id': '$method', 'sum': {'$sum': 1}}}
+    ]
+    print("Methods:")
+    result = collection.aggregate(pipeline)
+    for doc in result:
+        if doc['_id'] in reqs:
+            req_dic[doc['_id']] = doc['sum']
+    for req in reqs:
+        print(f"\tmethod {req}: {req_dic[req]}")
+    st_checks = collection.count_documents({"method": 'GET',
+                                            "path": "/status"})
+    print(f"{st_checks} status check")
